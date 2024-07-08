0fc2543b6b57b73f40ccc031af7996a5c3ed191d
Nanthawat45 <Nanthawat.ohm45@gmail.com>
2024-07-08 17:40:02 +0700

    bugfix-db.connect

---
+++ config/db.config.js
@@ -0,0 +1,15 @@
+require ("dotenv").config();
+
+module.exports = {
+  HOST: "ep-empty-frog-a15ro460-pooler.ap-southeast-1.aws.neon.tech",
+  USER: "default",
+  PASSWORD: "nFYZ8GVXsy0U",
+  DB: "verceldb",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
