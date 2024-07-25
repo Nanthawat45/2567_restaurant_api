f0453fd86e605e0854d43c2a0328208ea9a99fc0
Nanthawat45 <Nanthawat.ohm45@gmail.com>
2024-07-25 16:11:58 +0700

    25/07/67

--- routers/restaurant.router.js
+++ routers/restaurant.router.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const restaurantController = require("../controllers/restaurant.controller");
+const { authJwt } = require("../middleware");
 
 //create a restaurant
 //POST http://localhost:5000/api/v1/restaurants/
-router.post("/", restaurantController.create);
+router.post(
+  "/",
+  [authJwt.verifyToken, authJwt.isAdminOrMod],
+  restaurantController.create
+);
 
 //Get All restaurants
 router.get("/", restaurantController.getAll);
 
 //Get restaurant by Id
-router.get("/:id", restaurantController.getById);
+router.get("/:id", [authJwt.verifyToken], restaurantController.getById);
 
+//update a retaurant
+router.get(
+  "/:id",
+  [authJwt.verifyToken, authJwt.isAdminOrMod],
+  restaurantController.update
+);
+
+//delete a restaurant
+router.get(
+  "/:id",
+  [authJwt.verifyToken, authJwt.isAdmin],
+  restaurantController.delete
+);
 module.exports = router;
